d4592ce73af9bf548ada134b58b99550eb9396fb
Artsy! <84583236+Artsy-Smartsy@users.noreply.github.com>
2023-01-30 18:27:31 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAAXSYnk7Q-xDWBj8UoE1r1gwfJjqx588E",
+  authDomain: "tvapp-aec7d.firebaseapp.com",
+  projectId: "tvapp-aec7d",
+  storageBucket: "tvapp-aec7d.appspot.com",
+  messagingSenderId: "1015109606311",
+  appId: "1:1015109606311:web:d9ff50b7c1a093291ff482"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
